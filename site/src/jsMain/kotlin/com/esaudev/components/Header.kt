ed0eeeb413702dcffc448763c316f29a439c3a6b
Esau <yosoypawn@hotmail.com>
2023-04-15 23:11:18 -0600

    feat: Main section

---
+++ site/src/jsMain/kotlin/com/esaudev/components/Header.kt
@@ -0,0 +1,92 @@
+package com.esaudev.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.esaudev.models.Section
+import com.esaudev.models.Theme
+import com.esaudev.styles.LogoStyle
+import com.esaudev.styles.NavigationItemStyle
+import com.esaudev.util.Constants.FONT_FAMILY
+import com.esaudev.util.Res
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.css.TextDecorationLine
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.fa.FaBars
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun Header() {
+    val breakpoint by rememberBreakpoint()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(
+                if (breakpoint > Breakpoint.MD) 80.percent else 90.percent
+            )
+            .margin(topBottom = 50.px),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LeftSide(breakpoint = breakpoint)
+        if (breakpoint > Breakpoint.MD) {
+            RightSide()
+        }
+    }
+}
+
+@Composable
+fun LeftSide(breakpoint: Breakpoint) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (breakpoint <= Breakpoint.MD) {
+            FaBars(
+                modifier = Modifier.margin(right = 15.px),
+                size = IconSize.XL
+            )
+        }
+        Image(
+            modifier = LogoStyle.toModifier()
+                .height(120.px)
+                .width(180.px),
+            src = Res.Image.Logo,
+            desc = "Logo image"
+        )
+    }
+}
+
+@Composable
+fun RightSide() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .borderRadius(r = 50.px)
+            .backgroundColor(Theme.LighterGray.rgb)
+            .padding(20.px),
+        horizontalArrangement = Arrangement.End
+    ) {
+        Section.values().take(6).forEach { section ->
+            Link(
+                modifier = NavigationItemStyle.toModifier()
+                    .padding(right = 30.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(18.px)
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
+                path = section.path,
+                text = section.title
+            )
+        }
+    }
+}
---
+++ site/src/jsMain/kotlin/com/esaudev/util/Constants.kt
@@ -0,0 +1,18 @@
+package com.esaudev.util
+
+object Constants {
+    const val SECTION_WIDTH = 1920
+    const val FONT_FAMILY = "Helvetica"
+
+    const val LOREM_IPSUM_SHORTEST = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut sapien" +
+            " placerat diam molestie blandit eget at sapien. Nam venenatis purus hendrerit tincidunt accumsan. Aenean" +
+            " augue turpis, consequat ut blandit sit amet, ultrices facilisis neque."
+}
+
+object Res {
+    object Image {
+        const val Background = "background.svg"
+        const val Logo = "android_logo.svg"
+        const val AndroidAndy = "android_andy.png"
+    }
+}
