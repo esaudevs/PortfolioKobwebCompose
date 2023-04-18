5bad378fd1674d4bc55ab50ebe210b8744951201
Esau <yosoypawn@hotmail.com>
2023-04-18 10:25:09 -0600

    feat: Service section

--- site/src/jsMain/kotlin/com/esaudev/pages/Index.kt
+++ site/src/jsMain/kotlin/com/esaudev/pages/Index.kt
@@ -3,6 +3,7 @@ package com.esaudev.pages
 import androidx.compose.runtime.*
 import com.esaudev.sections.AboutSection
 import com.esaudev.sections.MainSection
+import com.esaudev.sections.ServiceSection
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -20,5 +21,6 @@ fun HomePage() {
     ) {
         MainSection()
         AboutSection()
+        ServiceSection()
     }
 }
--- site/src/jsMain/kotlin/com/esaudev/util/Constants.kt
+++ site/src/jsMain/kotlin/com/esaudev/util/Constants.kt
@@ -19,4 +19,18 @@ object Res {
         const val AndroidAndy = "android_andy.png"
         const val AboutImage = "about_image.png"
     }
+    object Icon {
+        const val web = "web_icon.svg"
+        const val seo = "seo_icon.svg"
+        const val design = "design_icon.svg"
+        const val business = "business_icon.svg"
+        const val apple = "apple_icon.svg"
+        const val android = "android_icon.svg"
+        const val link = "link_icon.svg"
+        const val user = "user_icon.svg"
+        const val shield = "shield_icon.svg"
+        const val happy = "happy_icon.svg"
+        const val checkmark = "checkmark_icon.svg"
+        const val star = "star_icon.svg"
+    }
 }
