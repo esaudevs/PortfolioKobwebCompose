92c2020ad768e865540aaa67cfe79fda48275e8a
Esau <yosoypawn@hotmail.com>
2023-05-19 14:36:37 -0600

    feat: Update with personal data

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kobweb.application)
-    alias(libs.plugins.kobwebx.markdown)
+    //alias(libs.plugins.kobwebx.markdown)
 }
 
 group = "com.esaudev"
@@ -31,8 +31,8 @@ kobweb {
 }
 
 kotlin {
-    configAsKobwebApplication("esaudev", includeServer = true)
-    jvmToolchain(11) // Kobweb server should use at least Java 11
+    configAsKobwebApplication("esaudev")
+    //jvmToolchain(11) // Kobweb server should use at least Java 11
 
     @Suppress("UNUSED_VARIABLE") // Suppress spurious warnings about sourceset variables not being used
     sourceSets {
@@ -48,13 +48,13 @@ kotlin {
                 implementation(libs.kobweb.core)
                 implementation(libs.kobweb.silk.core)
                 implementation(libs.kobweb.silk.icons.fa)
-                implementation(libs.kobwebx.markdown)
+                //implementation(libs.kobwebx.markdown)
              }
         }
-        val jvmMain by getting {
+        /*val jvmMain by getting {
             dependencies {
                 implementation(libs.kobweb.api)
              }
-        }
+        }*/
     }
 }
