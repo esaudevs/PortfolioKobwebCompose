26ba18fe38f31d281ab6a7cba708fbed33caf556
Esau <yosoypawn@hotmail.com>
2023-05-19 10:30:01 -0600

    feat: Section title animations

--- site/src/jsMain/kotlin/com/esaudev/components/SectionTitle.kt
+++ site/src/jsMain/kotlin/com/esaudev/components/SectionTitle.kt
@@ -1,9 +1,11 @@
 package com.esaudev.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.esaudev.models.Section
 import com.esaudev.models.Theme
 import com.esaudev.util.Constants
+import com.esaudev.util.ObserveViewPortEntered
+import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Box
@@ -12,6 +14,10 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.ms
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -22,6 +28,25 @@ fun SectionTitle(
     section: Section,
     alignment: Alignment.Horizontal = Alignment.Start,
 ) {
+
+    val scope = rememberCoroutineScope()
+    var titleMargin by remember { mutableStateOf(50.px) }
+    var subtitleMargin by remember { mutableStateOf(50.px) }
+
+    ObserveViewPortEntered(
+        sectionId = section.id,
+        distanceFromTop = 700.0,
+        onViewPortEntered = {
+            scope.launch {
+                subtitleMargin = 0.px
+                if (alignment == Alignment.Start) {
+                    delay(25)
+                }
+                titleMargin = 0.px
+            }
+        }
+    )
+
     Column(
         modifier = modifier,
         horizontalAlignment = alignment
@@ -36,11 +61,16 @@ fun SectionTitle(
                         else -> TextAlign.Start
                     }
                 )
-                .margin(topBottom = 0.px)
+                .margin(
+                    top = 0.px,
+                    bottom = 0.px,
+                    left = titleMargin
+                )
                 .fontFamily(Constants.FONT_FAMILY)
                 .fontSize(25.px)
                 .fontWeight(FontWeight.Normal)
                 .color(Theme.Primary.rgb)
+                .transition(CSSTransition(property = "margin", duration = 300.ms))
                 .toAttrs()
         ) {
             Text(section.title)
@@ -56,11 +86,17 @@ fun SectionTitle(
                         else -> TextAlign.Start
                     }
                 )
-                .margin(bottom = 10.px, top = 0.px)
+                .margin(
+                    bottom = 10.px,
+                    top = 0.px,
+                    left = if (alignment == Alignment.Start) subtitleMargin else 0.px,
+                    right = if (alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px
+                )
                 .fontFamily(Constants.FONT_FAMILY)
                 .fontSize(40.px)
                 .fontWeight(FontWeight.Bold)
                 .color(Theme.Secondary.rgb)
+                .transition(CSSTransition(property = "margin", duration = 300.ms))
                 .toAttrs()
         ) {
             Text(section.subtitle)
